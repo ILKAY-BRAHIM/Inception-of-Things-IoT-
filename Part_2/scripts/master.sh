4fa1c998d6f25ca8acde43a5673bdd0712dee2da
brahim <brahim@debian>
2024-01-16 06:42:06 -0500

    finish part2

--- Part_2/scripts/master.sh
+++ Part_2/scripts/master.sh
@@ -2,13 +2,11 @@
 
 echo "[K3S] : installing k3s-server..."
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -
-# sleep 25
-# echo "hi......"
-# sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-# sudo cat /var/lib/rancher/k3s/server/node-token
-# kubectl apply -n default -f /vagrant/app1.yaml
-# kubectl apply -n default -f /vagrant/ingress.yaml
-# sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/
-# sudo cp /etc/rancher/k3s/k3s.yaml /vagrant/
+sleep 25
+echo "deploment apps..."
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+kubectl apply -n default -f /vagrant/app1.yaml
+kubectl apply -n default -f /vagrant/app2.yaml
+kubectl apply -n default -f /vagrant/app3.yaml
 
 # /usr/local/bin/k3s-uninstall.sh
